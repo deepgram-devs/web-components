24aded9c2db34fb6831116007e205ae108eae380
Luke Oliff <luke@lukeoliff.com>
2023-01-27 17:22:10 +0000

    calling it a day with icons and svgs for the moment

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-export { default as Header } from './Header.astro'
-export { default as Footer } from './Footer.astro'
-export { default as Icon } from './Icon.astro'
-export { default as Svg } from './Svg.astro'
-export { default as Link } from './Link.astro'
-export { default as PrimarySection } from './Sections/PrimarySection.astro'
-export { default as HasAside } from './SectionOptions/HasAside.astro'
-export { default as HasGutters } from './SectionOptions/HasGutters.astro'
+export { default as Navbar } from './components/Navbar/Navbar.astro'
+
+export { default as Footer } from './components/Footer.astro'
+export { default as Icon } from './components/Icon.astro'
+export { default as Svg } from './components/Svg.astro'
+export { default as Link } from './components/Link.astro'
+
+export { default as PrimarySection } from './components/Sections/PrimarySection.astro'
+
+export { default as HasAside } from './components/SectionOptions/HasAside.astro'
+export { default as HasGutters } from './components/SectionOptions/HasGutters.astro'
+
+// export {}
